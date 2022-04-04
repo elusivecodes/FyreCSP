972a26715114fbf481c4f983ff595af77f7677ea
Elusive <elusivecodes@gmail.com>
2022-04-04 18:17:49 +1000

    Removed FyreError dependency

--- src/Exceptions/CspException.php
+++ src/Exceptions/CspException.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 namespace Fyre\CSP\Exceptions;
 
 use
-    Fyre\Error\Exceptions\Exception;
+    RuntimeException;
 
 /**
  * CspException
  */
-class CspException extends Exception
+class CspException extends RuntimeException
 {
 
     public static function forInvalidDirective(string $directive)
